2caf5f3db788143d654ed31cc3449e761f3b419c
Cale Sumner <calecsumner@gmail.com>
2024-11-14 13:57:43 -0700

    front end through backend

--- server/routes/main.js
+++ server/routes/main.js
@@ -11,8 +11,50 @@ router.get("/", async (req, res) => {
   };
 
   try {
-    const data = await Post.find().sort({ createdAt: "desc" });
+    const data = await Post.find().sort({ title: "desc" });
     res.render("index", { locals, data });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// get Post by Id
+router.get("/post/:id", async (req, res) => {
+  try {
+    let slug = req.params.id;
+
+    const data = await Post.findById({ _id: slug });
+
+    const locals = {
+      title: data.title,
+      description:
+        "A Blog template application that will be used for your own use.",
+    };
+    res.render("post", { locals, data });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// Search route
+router.get("/search", async (req, res) => {
+  try {
+    const locals = {
+      title: "Search",
+      description: "A Blog template made with nodeJS and ExpressJS",
+    };
+
+    let searchTerm = req.body.SearchTerm;
+    const searchNoSpecialChar = searchTerm.replace(/[^a-zA-z ]/g, "");
+
+    const data = await Post.find({
+      $or: [
+        { title: { $regex: new RegExp(searchNoSpecialChar, "i") } },
+        { body: { $regex: new RegExp(searchNoSpecialChar, "i") } },
+      ],
+    });
+
+    res.render("search", { locals, data });
   } catch (error) {
     console.log(error);
   }
