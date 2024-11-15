7545d6b5ea64c51c07c273f4813627bd73e2aba7
Cale Sumner <calecsumner@gmail.com>
2024-11-15 12:52:07 -0700

    blog site working

--- public/js/script.js
+++ public/js/script.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
   const allButtons = document.querySelectorAll(".searchBtn");
-  const searchBar = document.querySelectorAll(".searchBar");
-  const searchInput = document.querySelectorAll("searchInput");
-  const searchClose = document.querySelectorAll("searchClose");
+  const searchBar = document.querySelector(".searchBar");
+  const searchInput = document.getElementById("searchInput");
+  const searchClose = document.getElementById("searchClose");
 
   for (var i = 0; i < allButtons.length; i++) {
     allButtons[i].addEventListener("click", function () {
